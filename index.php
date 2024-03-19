6be60c4d3d74db34dd0e67eef1bf6b8c8c461189
Willyam Mach <willyam.mach@gmail.com>
2024-03-19 15:04:02 -0300

    Definição de design e começo da alocação flex

--- index.php
+++ index.php
@@ -3,29 +3,55 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="css/styles.ccs">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <title>Agenda - Buddemeyer</title>
 </head>
-<body>
-    <form action="">
-        <label>Nome *</label>
-        <input type="text"/>
+<body>  
+    <div class="layout">
+        <section class="dadosInputSection">
+            <form>
+                <div class="campoNome">
+                    <label>Nome *</label>
+                    <input type="text" placeholder="Seu Nome" required/>
+                </div>
 
-        <label>Endereço *</label>
-        <input type="text"/>
+                <div class="campoNome">
+                    <label>Endereço *</label>
+                    <input type="text" placeholder="Seu Endereço" required/>
+                </div>
 
-        <label>Cidade *</label>
-        <input type="text"/>
+                <div class="campoNome">
+                    <label>Cidade *</label>
+                    <input type="text" placeholder="Sua Cidade" required/>
+                </div>  
+                
+                <div class=" campoNome">
+                    <label>Estado *</label>
+                    <input type="text" placeholder="Seu Estado" required/>
+                </div>
 
-        <label>Estado *</label>
-        <input type="text"/>
+                <div class="campoNome">
+                    <label>E-mail *</label>
+                    <input type="email" placeholder="exemplo@email.com" required/>
+                </div>
+            
+                <div class="campoNome">
+                    <label for="">Telefone *</label>
+                    <input type="tel" name="telefone" placeholder="99 99999-9999" required/>
+                </div>
 
-        <label>E-mail *</label>
-        <input type="email"/>
-
-        <label>Telefone *</label>
-        <input type="number/>
-
-    </form>
+                <input class="campoEnviar" type="submit" value="Adicionar">
+            </form>
+        </section>
+            
+        <section class="agendaSection"> 
+            <h1>ola</h1>
+        </section>
+        
+    </div>
+    
 </body>
 </html>
