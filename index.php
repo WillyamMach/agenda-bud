c6da44365e6653e3383c270d07995ca6def390c6
Willyam Mach <willyam.mach@gmail.com>
2024-03-19 13:16:31 -0300

    Criar página base

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="css/styles.ccs">
+    <title>Agenda - Buddemeyer</title>
+</head>
+<body>
+    <form action="">
+        <label>Nome</label>
+        <input type="text"/>
 
+        <label>Endereço</label>
+        <input type="text"/>
+
+        <label>Cidade</label>
+        <input type="text"/>
+
+        <label>Estado</label>
+        <input type="text"/>
+
+        <label>E-mail</label>
+        <input type="email"/>
+
+        <label>Telefone</label>
+        <input type="number"/>
+
+    </form>
+</body>
+</html>
