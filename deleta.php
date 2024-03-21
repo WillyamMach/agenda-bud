b3df9e01bebf66af54facf1597a403e8ba65b606
Willyam Mach <willyam.mach@gmail.com>
2024-03-21 14:45:34 -0300

    Está com erro, revisar em casa e tentar resolver amanhã

--- deleta.php
+++ deleta.php
@@ -1,11 +1,10 @@
 <?php 
-    // require 'config.php';
+    require 'config.php';
 
-    // if($_SERVER["REQUEST_METHOD"] === 'POST'){
-
-    //     $deletaDados = ("DELETE FROM tb_informacoes WHERE inf_id=");
-    //     $preparaRemove = $mysql->prepare($deletaDados);
-    //     $preparaRemove->execute();
+    if($_SERVER["REQUEST_METHOD"] === 'POST'){
+        $deletaDados = ("DELETE FROM tb_informacoes WHERE inf_id= $id");
+        $preparaRemove = $mysql->prepare($deletaDados);
+        $preparaRemove->execute();
 
         header("Location: index.php");
-    // }
+    }
