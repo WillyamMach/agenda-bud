40322a463a04572bf9396c1db50491c49a565f07
Willyam Mach <willyam.mach@gmail.com>
2024-03-25 08:44:33 -0300

    Change 'editaDados' to 'editaAtulizaDados'

--- editaAtualizaDados.php
+++ editaAtualizaDados.php
@@ -9,7 +9,8 @@
         $estado = $_POST['estado'];
         $email = $_POST['email'];
         $telefone = $_POST['telefone'];
-   
+        
+        // reaproveitando o POST para utilizar update e insert
         if($id){
             $query = ("UPDATE tb_informacoes SET inf_nome = '$nome', inf_endereco = '$endereco', inf_cidade = '$cidade', inf_estado = '$estado', inf_email = '$email', inf_telefone = '$telefone' WHERE inf_id = $id");
         } else { 
