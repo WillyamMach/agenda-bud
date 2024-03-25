035c29cf5cc0a9d71ddd5bfa79f90fcbbdf2162b
Willyam Mach <willyam.mach@gmail.com>
2024-03-25 14:12:16 -0300

    Criar funcionalidade que aceita somente numeros e alguns caracteres no input tel

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function somenteNumeros(valor) {
+    var caractere = /[^0-9-() $]/;
+    var caractereInput = valor;
+    if (caractere.test(caractereInput.value)) {
+        caractereInput.value = "";
+    }
+}
