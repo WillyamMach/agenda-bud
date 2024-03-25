b9447600d1b35429ae4bed44fe009b6904c5d0ea
Willyam Mach <willyam.mach@gmail.com>
2024-03-25 14:11:47 -0300

    Ajeitar espaçamentos

--- mostrarDados.php
+++ mostrarDados.php
@@ -1,5 +1,7 @@
 <?php 
-    $resultado = $mysql->query("SELECT inf_id, inf_nome, inf_endereco, inf_cidade, inf_estado, inf_email, inf_telefone FROM tb_informacoes");
+    require 'config.php';
+
+    $resultado = $mysql->query("SELECT inf_id, inf_nome, inf_endereco, inf_cidade, inf_estado, inf_email, concat('(',substr(inf_telefone,1,2),') ',substr(inf_telefone,3,5),'-',substr(inf_telefone,8)) FROM tb_informacoes");
     $infoDb = $resultado->fetch_all(MYSQLI_ASSOC);
 
  ?>
